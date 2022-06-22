104828c6fa4afdbb9e292c515e5ae8177cd5e9ab
MERCYSUCCESS <105282345+MERCYSUCCESS@users.noreply.github.com>
2022-06-22 22:04:58 +0100

    Add files via upload

---
+++ Introduction to Javascript/app.js
@@ -0,0 +1,8 @@
+ var name, country, height
+name = prompt("Enter your name")
+country=prompt("What country are you from?: ")
+height=prompt("Enter your Height: ")
+
+document.write("My name is " + name + "<br>" + "My Country is "
+    + country + "<br>" + "And my height is "
++ height +"ft")
